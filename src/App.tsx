25ee8e37f517a55fe1637217e304ad8bef20daa4
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-09-21 08:22:54 -0300

    chore: add tailwind css setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [toggleTheme, setToggleTheme] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setToggleTheme((prevToggle) => !prevToggle);
+  };
+
   return (
     <>
-      <div>
-        <h1>Planit</h1>
+      <div
+        className={`${toggleTheme ? "flex justify-center items-center h-[100vh] w-full bg-slate-700" : "flex justify-center items-center h-[100vh] w-full bg-black"}`}
+      >
+        <h1
+          onClick={handleClick}
+          className="text-xl text-white p-3 cursor-pointer bg-green-500 rounded-sm hover:bg-green-600 transition-all duration-[0.2s]"
+        >
+          Planit
+        </h1>
       </div>
     </>
   );
