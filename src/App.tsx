193cc9312d6f1e9b8d6d84c7e5f782eaae186fe8
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-09-22 17:11:06 -0300

    feat: add Sidebar and SidebarButton Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { useState } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  const [toggleTheme, setToggleTheme] = useState<boolean>(false);
-
-  const handleClick = () => {
-    setToggleTheme((prevToggle) => !prevToggle);
-  };
-
   return (
     <>
-      <div
-        className={`${toggleTheme ? "flex h-[100vh] w-full items-center justify-center bg-slate-700" : "flex h-[100vh] w-full items-center justify-center bg-black"}`}
-      >
-        <button
-          onClick={handleClick}
-          className="cursor-pointer rounded-sm bg-green-500 p-3 text-xl text-white transition-all duration-[0.2s] hover:bg-green-600"
-        >
-          Planit
-        </button>
-      </div>
+      <Sidebar />
     </>
   );
 }
---
+++ src/components/SidebarButton/SidebarButton.tsx
@@ -0,0 +1,20 @@
+interface SidebarButtonProps {
+  children: React.ReactNode;
+  variant: "unselected" | "selected";
+}
+
+const SidebarButton = ({ children, variant }: SidebarButtonProps) => {
+  const getVariantClasses = () => {
+    if (variant === "unselected") return "text-[#35383e]";
+
+    if (variant === "selected") return "bg-[#e6f7f8] text-[#00adb5]";
+  };
+
+  return (
+    <a className={`${getVariantClasses()} rounded-lg px-6 py-3`} href="#">
+      {children}
+    </a>
+  );
+};
+
+export default SidebarButton;
