a9e9a1024bbea5ec4c18d3e8383e14a824dc1af9
Hialagol <hiagosilvaanjos7@gmail.com>
2024-09-26 13:25:29 -0300

    feat: add separator between task

--- src/assets/IconsComponents.tsx
+++ src/assets/IconsComponents.tsx
@@ -109,3 +109,166 @@ export const TrashIcon = () => {
     </svg>
   );
 };
+
+export const CloudSunIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_51_122)">
+        <path
+          d="M10 2.44672V4.11338"
+          stroke="#9A9C9F"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M4.1084 4.88837L5.2834 6.06337"
+          stroke="#9A9C9F"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M16.6667 10.78H18.3334"
+          stroke="#9A9C9F"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M15.8916 4.88837L14.7166 6.06337"
+          stroke="#9A9C9F"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M13.2891 11.3217C13.3916 10.7005 13.3161 10.0629 13.0713 9.48275C12.8265 8.90263 12.4224 8.40372 11.9057 8.04377C11.3891 7.68383 10.7811 7.47758 10.1521 7.4489C9.52308 7.42021 8.89881 7.57028 8.35156 7.88171"
+          stroke="#9A9C9F"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M10.8333 19.1134H5.8333C5.04494 19.1132 4.2728 18.8894 3.60657 18.4679C2.94033 18.0464 2.40734 17.4446 2.06949 16.7323C1.73164 16.02 1.6028 15.2265 1.69793 14.4439C1.79307 13.6613 2.10827 12.9217 2.60693 12.3111C3.1056 11.7005 3.76726 11.2439 4.51507 10.9943C5.26288 10.7448 6.06615 10.7125 6.83158 10.9012C7.59702 11.0899 8.29321 11.4919 8.8393 12.0605C9.38539 12.6291 9.75896 13.3409 9.91663 14.1134H10.8333C11.4963 14.1134 12.1322 14.3768 12.6011 14.8456C13.0699 15.3144 13.3333 15.9503 13.3333 16.6134C13.3333 17.2764 13.0699 17.9123 12.6011 18.3811C12.1322 18.85 11.4963 19.1134 10.8333 19.1134Z"
+          stroke="#9A9C9F"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_51_122">
+          <rect
+            width="20"
+            height="20"
+            fill="white"
+            transform="translate(0 0.780029)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export const MoonIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 2.56C9.00544 3.55456 8.4467 4.90347 8.4467 6.31C8.4467 7.71652 9.00544 9.06544 10 10.06C10.9946 11.0546 12.3435 11.6133 13.75 11.6133C15.1565 11.6133 16.5054 11.0546 17.5 10.06C17.5 11.5434 17.0601 12.9934 16.236 14.2268C15.4119 15.4601 14.2406 16.4214 12.8701 16.9891C11.4997 17.5568 9.99168 17.7053 8.53683 17.4159C7.08197 17.1265 5.7456 16.4122 4.6967 15.3633C3.64781 14.3144 2.9335 12.978 2.64411 11.5232C2.35472 10.0683 2.50325 8.56032 3.07091 7.18987C3.63856 5.81943 4.59986 4.64809 5.83323 3.82398C7.0666 2.99987 8.51664 2.56 10 2.56Z"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export const SunIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.3333 12.6667C12.1743 12.6667 13.6667 11.1743 13.6667 9.33333C13.6667 7.49238 12.1743 6 10.3333 6C8.49238 6 7 7.49238 7 9.33333C7 11.1743 8.49238 12.6667 10.3333 12.6667Z"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10 2.16666V3.83332"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10 17.1667V18.8333"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.1084 4.60834L5.2834 5.78334"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.7166 15.2167L15.8916 16.3917"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M1.66675 10.5H3.33341"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16.6667 10.5H18.3334"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5.2834 15.2167L4.1084 16.3917"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.8916 4.60834L14.7166 5.78334"
+        stroke="#9A9C9F"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,15 +1,21 @@
-import { AddIcon, TrashIcon } from "../../assets/IconsComponents";
+import {
+  AddIcon,
+  CloudSunIcon,
+  MoonIcon,
+  SunIcon,
+  TrashIcon,
+} from "../../assets/IconsComponents";
 import Button from "../Button/Button";
 
 const Tasks = () => {
   return (
-    <div className="w-full px-8 py-16">
+    <div className="w-full space-y-4 px-8 py-16">
       <div className="flex w-full justify-between">
         <div>
           <span className="text-xs font-semibold text-[#00adb8]">
             Minhas Tarefas
           </span>
-          <h2 className="text-xl font-semibold">Tasks</h2>
+          <p className="text-xl font-semibold">Tasks</p>
         </div>
         <div className="flex items-center gap-3">
           <Button variant="ghost">
@@ -20,6 +26,32 @@ const Tasks = () => {
             Adicionar tarefa
             <AddIcon />
           </Button>
+        </div>
+      </div>
+
+      <div className="rounded-xl bg-white p-6">
+        {/* MANHÂ */}
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-[#f4f4f5] pb-3">
+            <SunIcon />
+            <p className="text-xs text-[#9a9c9f]">Manhâ</p>
+          </div>
+        </div>
+
+        {/* TARDE */}
+        <div className="my-6 space-y-6">
+          <div className="flex gap-2 border-b border-[#f4f4f5] pb-3">
+            <CloudSunIcon />
+            <p className="text-xs text-[#9a9c9f]">Tarde</p>
+          </div>
+        </div>
+
+        {/* NOITE */}
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-[#f4f4f5] pb-3">
+            <MoonIcon />
+            <p className="text-xs text-[#9a9c9f]">Noite</p>
+          </div>
         </div>
       </div>
     </div>
