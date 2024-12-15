153d163173c90356b016223dc8dde648d9b2f438
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-12-15 16:19:51 -0300

    feat: ensuring that task is deleted on db

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -46,7 +46,14 @@ const Tasks = () => {
     setAddTaskDialogIsOpen(false);
   };
 
-  const handleTasksDeleteClick = (taskId: string) => {
+  const handleTasksDeleteClick = async (taskId: string) => {
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok)
+      return toast.error("Erro ao deletar tarefa. Por favor, tente novamente.");
+
     const newTasks = tasks.filter((task) => task.id != taskId);
     setTasks(newTasks);
     toast.success("Tarefa removida com sucesso!");
